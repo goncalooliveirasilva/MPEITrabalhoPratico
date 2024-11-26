9bacc20b5a131a821509056a41c27abd4216fa37
Apmds <ap1mrfj@gmail.com>
2024-11-26 23:19:40 +0000

    Nada importante

---
+++ binarizeImages.m
@@ -0,0 +1,40 @@
+function binarizeImages(imageFolder, outputFolder)
+    % Esta função converte as imagens png de um diretório, 
+    % binariza-as e guarda-as num novo diretório
+    % == Argumentos ==
+    % imageFolder: path para o diretório das imagens a converter
+    % outputFolder: path para o diretório de destino
+    imageFiles = dir(fullfile(imageFolder, '*.jpg'));
+
+    targetSize = [100 NaN];
+    if isempty(imageFiles)
+        error('No images found in the specified folder.');
+    end
+    if ~exist(outputFolder, 'dir')
+        mkdir(outputFolder);
+    else
+        delete(fullfile(outputFolder, '*.jpg'));
+    end
+    for k=1:length(imageFiles)
+        filePath = fullfile(imageFolder, imageFiles(k).name);
+        I = imread(filePath);
+        I = imresize(I, targetSize);
+        if size(I, 3) == 3
+            I = rgb2gray(I);
+        end
+        BW = imbinarize(I);
+        if mean(BW(:)) > 0.5
+            BW = ~BW;
+        end
+        [~, name, ext] = fileparts(imageFiles(k).name);
+        outputFile = fullfile(outputFolder, [name, '_binarized', '.png']);
+        imwrite(BW, outputFile);
+    end
+    n1 = length(imageFiles);
+    n2 = length(dir(fullfile(outputFolder, '*.png')));
+    if n1 ~= n2
+        fprintf('Warning: %d images were not binarized!\n', n1-n2);
+    else
+        fprintf('All %d images were binarized!\n', n1);
+    end
+end
---
+++ main.m
@@ -0,0 +1,6 @@
+clc
+binarizeImages("dataset_old/train/circle/", "dataset/train/circle/")
+binarizeImages("dataset_old/train/triangle/", "dataset/train/triangle/")
+binarizeImages("dataset_old/train/rectangle/", "dataset/train/rectangle/")
+binarizeImages("dataset_old/train/square/", "dataset/train/square/")
+binarizeImages("dataset_old/train/kite/", "dataset/train/kite/")
