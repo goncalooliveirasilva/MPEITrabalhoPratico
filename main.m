826874afb5cd741dd9703e6d884bfa58f4fc653f
Apmds <ap1mrfj@gmail.com>
2024-12-02 00:13:05 +0000

    Dados de treino guardados + função de obter probabilidades para naive bayes

--- main.m
+++ main.m
@@ -1,5 +1,11 @@
 clc
 
+% binarizeImages("dataset_old/test/circle/", "dataset/test/circle/")
+% binarizeImages("dataset_old/test/triangle/", "dataset/test/triangle/")
+% binarizeImages("dataset_old/test/rectangle/", "dataset/test/rectangle/")
+% binarizeImages("dataset_old/test/square/", "dataset/test/square/")
+% binarizeImages("dataset_old/test/kite/", "dataset/test/kite/")
+
 function [imageData, categories] = loadImageData(path)
     dirs = dir(path);
     dirflag = [dirs.isdir];
@@ -9,6 +15,8 @@ function [imageData, categories] = loadImageData(path)
     
     imageData = zeros(1, 10000);
     categories = categorical(1, 10000);
+
+    index = 0;
     for i=1:length(categories_names)
         cat_name = convertCharsToStrings(categories_names{i});
         directory = join([path categories_names{i} "/"], "");
@@ -16,21 +24,34 @@ function [imageData, categories] = loadImageData(path)
         imageFiles = dir(fullfile(directory, "*.png"));
         
         for k=1:length(imageFiles)
+            index = index + 1;
+
             filepath = fullfile(directory, imageFiles(k).name);
             imdata = imread(filepath);
             imdata = imdata(:)';
-            imageData(k + (i-1)*1500, :) = imdata;
-            categories(k + (i-1)*1500) = cat_name;
+            imageData(index, :) = imdata;
+            categories(index) = cat_name;
         end
     end
 end
 
 
-% [imageData, categories] = loadImageData("dataset/train/");
-save("imageTrainData.mat", "imageData", "categories")
+% [imageData, categories] = loadImageData("dataset/test/");
+% save("imageTestData.mat", "imageData", "categories")
 
-% binarizeImages("dataset_old/train/circle/", "dataset/train/circle/")
-% binarizeImages("dataset_old/train/triangle/", "dataset/train/triangle/")
-% binarizeImages("dataset_old/train/rectangle/", "dataset/train/rectangle/")
-% binarizeImages("dataset_old/train/square/", "dataset/train/square/")
-% binarizeImages("dataset_old/train/kite/", "dataset/train/kite/")
+function [categories_unique, base_probs, probs] = getProbabilities(imageData, categories)
+    % Probabilidades de obter categoria X
+    categories_unique = unique(categories);
+    base_probs = zeros(size(categories_unique));
+
+    probs = zeros(length(categories_unique), size(imageData, 2));
+    for i=1:length(categories_unique)
+        base_probs(i) = sum(categories == categories_unique(i))/length(categories);
+        
+        data = sum(imageData(categories == categories_unique(i), :));
+        probs(i, :) = data / sum(data);
+    end
+end
+
+
+[categories_unique, base_probs, probs] = getProbabilities(imageData, categories);
